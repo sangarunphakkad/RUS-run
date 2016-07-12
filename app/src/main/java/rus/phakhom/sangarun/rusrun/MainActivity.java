685d95f8fde70eacdbaf2546bc5b6efd1824c4f8
kad <sangarun-kad@hotmail.com>
2016-07-12 14:16:52 +0700

    Layout

--- app/src/main/java/rus/phakhom/sangarun/rusrun/MainActivity.java
+++ app/src/main/java/rus/phakhom/sangarun/rusrun/MainActivity.java
@@ -1,7 +1,9 @@
 package rus.phakhom.sangarun.rusrun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //Main Method
+
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this,SignUpActivity.class));
     }
-}
+
+}   // main Class นี่คือ คลาสหลัก น่ะค่ะ
